5bfc53824a5b0c517822ea690c637440c194b865
justtio <leviknows@gmial.com>
2019-11-01 22:08:28 +0800

    install redux and start learning

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import pokeball from "../pokeball.jpeg";
 
 class Home extends Component {
   state = {
@@ -18,8 +20,11 @@ class Home extends Component {
       posts.map(post => {
         return (
           <div className="post card" key={post.id}>
+            <img src={pokeball} alt="A pokeball" />
             <div className="card-content">
-              <span className="card-title">{post.title}</span>
+              <Link to={"/" + post.id}>
+                <span className="card-title purple-text">{post.title}</span>
+              </Link>
               <p>{post.body}</p>
             </div>
           </div>
