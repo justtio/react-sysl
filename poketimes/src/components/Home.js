9e36d7e6d5a16c01b62ccdd0d1054834c69ed399
justtio <leviknows@gmial.com>
2019-11-01 16:57:28 +0800

    added axios plugin

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>
-        This is the standard version that comes with both the minified and
-        unminified CSS and JavaScript files. This option requires little to no
-        setup. Use this if you are unfamiliar with Sass.
-      </p>
-    </div>
-  );
-};
+class Home extends Component {
+  state = {
+    posts: []
+  };
+  componentDidMount() {
+    axios.get("http://jsonplaceholder.typicode.com/posts").then(res => {
+      this.setState({
+        posts: res.data.slice(0, 10)
+      });
+    });
+  }
+  render() {
+    const { posts } = this.state;
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="card-title">{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      <div className="center">No posts yet!</div>
+    );
+
+    return (
+      <div className="container">
+        <h4 className="center">Home</h4>
+        {postList}
+      </div>
+    );
+  }
+}
 
 export default Home;
